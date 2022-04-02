cd8a0804332087e78d7155440091bf903ac2795c
tho4299 <93917461+tho4299@users.noreply.github.com>
2022-04-02 09:37:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Làm NY tớ đi",
-    introTitle: 'Khánh Huyền',ha
+    introTitle: 'Khánh Huyền',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
