ce860eb1aacde14d38ea59f987c6d4d6ced0572c
tho4299 <93917461+tho4299@users.noreply.github.com>
2022-05-22 16:33:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,7 @@
 const CONFIG = {
     titleWeb: "Em có ngốc không?",
     introTitle: 'Quỳnh Như',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Món quà gửi bé!!`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
